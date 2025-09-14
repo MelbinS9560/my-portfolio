f4a2f7875853a2f39d61955a065226573fa544c2
MelbinS9560 <melbingudalur123@gmail.com>
2025-09-14 21:11:57 +0530

    first successful pulish

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'   // <-- add this line
+  base: '/my-portfolio/'   // essential for GitHub Pages
 });
